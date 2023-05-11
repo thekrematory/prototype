89323f2d3febf89f17f6ba8b130b088dce0d547b
thekrematory <130934117+thekrematory@users.noreply.github.com>
2023-05-11 11:10:32 -0400

    Audio

---
+++ Prototypel/Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	[SerializeField] float mainThrustSpeed;
+	[SerializeField] float rotationSpeed;
+	[SerializeField] AudioClip mainThrustSound;
+
+	Rigidbody rb;
+	AudioSource audioS;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		audioS = GetComponent<AudioSource>();
+	}
+
+	void Update()
+	{
+		ProcessMainThrust();
+
+		ProcessRotation();
+	}
+
+	private void ProcessRotation()
+	{
+		if (Input.GetKey(KeyCode.A))
+		{
+			rb.freezeRotation = true;
+			transform.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+			rb.freezeRotation = false;
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			rb.freezeRotation = true;
+			transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+			rb.freezeRotation = false;
+		}
+	}
+
+	private void ProcessMainThrust()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			rb.AddRelativeForce(0, mainThrustSpeed * Time.deltaTime, 0);
+
+			if(!audioS.isPlaying) 
+			{
+				audioS.PlayOneShot(mainThrustSound);
+			}
+		}
+
+		else
+		{
+			audioS.Stop();
+		}
+	}
+}
